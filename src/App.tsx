251f888604d27bcb8a34c72d316c0d7cdebf02ce
Hoang Anh <hoanganh.luong2906@gmail.com>
2025-01-01 13:40:51 +0700

    🎉 [feat]: Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<main className='max-w-7xl mx-auto'>
+			<h1 className='text-red-500 text-5xl font-bold tracking-widest'>Portfolio</h1>
+		</main>
+	);
+};
+
+export default App;
